28334d77e18451106c93d872dc3d116e62db1123
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-09-17 00:39:53 +0530

    Complete

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const PORT=process.env.PORT||8000;
 
 const app=express();
 
+const Vonage = require('@vonage/server-sdk')
+
+const vonage = new Vonage({
+  apiKey: "",
+  apiSecret: ""
+})
+
+
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -16,6 +24,31 @@ app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
     res.render('main');
+});
+
+
+app.post('/',(req,res)=>{
+
+    const from = "Vonage APIs";
+    const to = req.body.number;
+    const text = req.body.message;
+
+    vonage.message.sendSms(from, to, text, (err, responseData) => {
+        if (err) {
+            console.log(err);
+        } 
+        else
+         {
+
+            if(responseData.messages[0]['status'] === "0") {
+               res.status(200).json({message:"Message sent successfully."});
+            } else {
+                res.status(400).json({message : `Message failed with error: ${responseData.messages[0]['error-text']}`});
+            }
+        }
+    });
+
+
 });
 
 // app.use(require('./routes/auth'));
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,31 @@
 const form=document.getElementById('form');
+const number=document.getElementById('number');
+const message=document.getElementById('message');
+const response=document.querySelector(".result");
+
+
+form.addEventListener('submit',(e)=>{
+    response.textContent="";
+    e.preventDefault();
+    const numberV=number.value.replace(/\D/g,'');
+    const messageV=message.value;
+
+    fetch('/',{
+        method:'POST',
+        headers:{
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify({
+            number:numberV,
+            message:messageV
+        
+        })
+    }).then(async (res)=>{
+        const data=await res.json();
+        response.textContent=data.message;
+
+        console.log(res);
+    }).catch((err)=>{
+        console.log(err);
+    });
+});
